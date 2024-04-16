5799d6a9390645adc28ba395792b4ce05793c9b1
LiteObject <Lite.Object@gmail.com>
2024-04-16 17:13:48 -0500

    Add CORS config

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000" # React app
+    "http://localhost:8000" # FastAPI server
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
